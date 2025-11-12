5416d06010230aa4c24dae23f532f1fc8b1fa897
Robert Pölzl <robert.poelzl@legero-united.com>
2025-11-12 13:09:31 +0100

    info updated

--- Luki Produkttexte.py
+++ Luki Produkttexte.py
@@ -157,7 +157,8 @@ with st.expander("Information"):
 
             st.markdown("""
                   <p>
-                  Willkommen in der App zur Automatischen Erstellung von Produkttexten. Diese wurde im Rahmen des LUKI-Projektes erstellt und generiert automatisch Texte für Modelle. Unter "Upload File" kannst du die zu verarbeitenden Modelle hochladen, das Excel muss dem Format des IPIM-Exportes "IPIM_Produkttexte" entsprechen. Die Verarbeitung in der App dauert einige Sekunden bis Minuten, dass sie arbeitet siehst du am Symbol am rechten oberen Bildschirmrand. 
+                  Willkommen in der App zur automatischen Erstellung von Produkttexten. Diese wurde im Rahmen des LUKI-Projektes erstellt und generiert automatisch Texte für Modelle. 
+                  Unter "Upload File" kannst du die zu verarbeitenden Modelle hochladen, das Excel muss dem Format des IPIM-Exportes "IPIM_Produkttexte" entsprechen. Die Verarbeitung in der App dauert einige Sekunden bis Minuten, dass sie arbeitet, siehst du am Symbol am rechten oberen Bildschirmrand. 
                   </p> <p>                 
                   Bitte lade in dieser Testphase nicht mehr als 50 Modelle auf einmal hoch. 
                   </p> <p>
