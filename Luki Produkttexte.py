49dfd2356238627002de7958472bfb26e0aebd5c
Andre Dörr <andre.doerr@areto.de>
2026-01-16 15:05:08 +0100

    a

--- Luki Produkttexte.py
+++ Luki Produkttexte.py
@@ -250,6 +250,8 @@ if df_org_data is not None:
         client = OpenAI(api_key=st.secrets["OPAI_KEYS"])
         rows_indx = 0
         list_output_data = []
+
+        st.write(df_org_data)
         
         # loop
         with st.spinner("Produkttexte werden generiert...", show_time=True):
