9896100ac6b559d89d4e2fbb18769c520ea273ab
Robert Poelzl <robert.poelzl@legero-united.com>
2026-01-29 10:42:54 +0100

    Model to gpt 5-mini

--- Luki Produkttexte.py
+++ Luki Produkttexte.py
@@ -25,7 +25,7 @@ st.set_page_config(
 
 # model select -> can be dynamic in the future with a dropbox
 # gpts_modl = "gpt-4.1-mini"
-gpts_modl = "gpt-5.2"
+gpts_modl = "gpt-5-mini"
 
 # input prompt -> can be dynamic in the future with a text box
 inpt_prmt = (
