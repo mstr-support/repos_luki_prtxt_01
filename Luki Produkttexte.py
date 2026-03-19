7ce68a95deece1129be53d5f727f0344b870a3de
Robert Poelzl <robert.poelzl@legero-united.com>
2026-03-19 09:29:49 +0100

    Abschlussatz geändert.

--- Luki Produkttexte.py
+++ Luki Produkttexte.py
@@ -46,17 +46,14 @@ inpt_prmt = (
     "Vermeide Aufzählungen, Wortwiederholungen, übermäßig werbliche Floskeln und direkte persönliche Ansprache."
     "Halte die Textlänge zwischen 500-550 Zeichen, erwähne nie das Wort Leisten."
     "Beachte korrekte Rechtschreibung und flüssigen Satzbau. Leistenname immer in Großbuchstaben"
-  #  "Hier ein Beispieltext: Ganz schön raffiniert, bewegt man sich mit der Sandale MOVE durch den Sommer. "
-  #  "Dezente Schmuckelemente an den Riemenenden, in Kombination mit dem naturgemilltem Nappaleder sorgen bei "
-  #  "dem legero Schuh für einen feinen und modernen Look. Die besonders weiche, flexible und superleichte PU-Sohle "
-  #  "mit dem markanten Profil macht MOVE so luftig und flexibel. Damit stellt sich das Sommergefühl ganz leicht ein. "
 )
 
 # Prompt for product text review
 inpt_prmt_review = (
     "Verbessere im folgenden Text Rechtschreib- und Grammatikfehler."
-    "Reduziere oder ersetze Wortwiederholungen, ohne den Inhalt zu verändern."
-    "Füge am Ende einen kurzen werbehaften Abschlusssatz hinzu, wie zB: Ein idealer Begleiter für die Stadt."
+    "Ersetze Wortwiederholungen, ohne den Inhalt zu verändern."
+    "Füge am Ende einen kurzen werbehaften Abschlusssatz hinzu, wie zB: Ein idealer Begleiter für die Stadt"
+    "oder Jetzt kann der Sommer kommen."
     "Gib ausschließlich den überarbeiteten Text zurück, ohne zusätzliche Erklärungen oder Kommentare."
     "Hier ein Beispieltext: Ganz schön raffiniert, bewegt man sich mit der Sandale MOVE durch den Sommer. "
     "Dezente Schmuckelemente an den Riemenenden, in Kombination mit dem naturgemilltem Nappaleder sorgen bei "
@@ -487,7 +484,7 @@ if df_org_data is not None:
                         {"role": "system", "content": "Du bist ein erfahrener Werbetexter für Schuhe."},
                         {"role": "user", "content": final_prompt}
                     ],
-                    temperature=0.6
+                    temperature=0.5
 
                     # tokens not needed for 5.2 model
                     #max_tokens=1200
@@ -545,7 +542,7 @@ if df_org_data is not None:
                         {"role": "system", "content": "Du überarbeitest Produkttexte sorgfältig und in natürlichem Deutsch."},
                         {"role": "user", "content": review_prompt}
                     ],
-                    temperature=0.9
+                    temperature=0.4
                 )
 
                 # Gore Tex in Ergebnis anpassen
