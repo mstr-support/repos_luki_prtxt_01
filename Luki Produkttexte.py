e87436dce3c271bf04f84f3756bef770112b2a9a
Robert Poelzl <robert.poelzl@legero-united.com>
2026-03-19 10:02:08 +0100

    modl test

--- Luki Produkttexte.py
+++ Luki Produkttexte.py
@@ -25,8 +25,8 @@ st.set_page_config(
 #
 
 # model select -> can be dynamic in the future with a dropbox
-gpts_modl = "gpt-5.4-mini"
-#gpts_modl = "gpt-5.4"
+#gpts_modl = "gpt-5.4-mini"
+gpts_modl = "gpt-5.4"
 
 
 
