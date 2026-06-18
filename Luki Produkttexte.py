2b82c2c99bad9bc1088cbf4617e7c673447e3a1b
Andre Dörr <andre.doerr@areto.de>
2026-06-18 14:00:56 +0200

    LEG-260

--- Luki Produkttexte.py
+++ Luki Produkttexte.py
@@ -468,6 +468,10 @@ def fnct_selling_points(row: pd.Series, marke: str) -> dict:
         val1_str = str(val1).strip()
         val2     = row[attr2] if (attr2 and attr2 in row.index) else None
 
+        st.write("Aktuelle Sellingpiont Menge:",len(results))
+        st.write("attr1:",attr1)
+        st.write("val1_str:",val1_str)
+
         # call selling point logic for attribute combination
         sp_text = fnct_selling_point(
             attribut1=attr1,
@@ -476,6 +480,8 @@ def fnct_selling_points(row: pd.Series, marke: str) -> dict:
             attribut2=attr2,
             wert2=val2
         )
+
+        st.write("sp_text:",sp_text)
 
         if sp_text and sp_text != val1_str:
             results.append(sp_text)
