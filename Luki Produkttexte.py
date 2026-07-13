7f33e80a18b631fbe122cc0a313edd19792e5427
Andre Dörr <andre.doerr@areto.de>
2026-07-13 14:23:06 +0200

    a

--- Luki Produkttexte.py
+++ Luki Produkttexte.py
@@ -422,6 +422,8 @@ def fnct_selling_point(
     # general filters
     #
 
+    st.dataframe(tab3_df)
+
     # filter selling point data only for relevant rows
     df_sp = st.session_state.tab3_df[st.session_state.tab3_df["Relevant"].astype(str).str.strip().str.upper() == "J"].copy()
 
