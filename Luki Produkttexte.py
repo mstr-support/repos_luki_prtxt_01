c79d528986309de71b4475a3d6efcaf41084c460
Andre Dörr <andre.doerr@areto.de>
2026-09-21 15:55:43 +0200

    a

--- Luki Produkttexte.py
+++ Luki Produkttexte.py
@@ -1174,13 +1174,7 @@ with tab2:
         # show data frame
         st.dataframe(tab2_df_org_data)
 
-        if "cnt_loop_1" in st.session_state:
-             st.write('loop 1',st.session_state.cnt_loop_1)
-
-        if "cnt_loop_2" in st.session_state:
-            st.write('loop 2',st.session_state.cnt_loop_2)
-
-
+        
         if st.session_state.tab2_generation_done:
             tab2_df_output_data = st.session_state.tab2_df_output_data
         
@@ -1385,6 +1379,11 @@ with tab2:
             st.session_state.tab2_timing = tab2_timing_records
             st.session_state.tab2_generation_done = True
 
+            if "cnt_loop_1" in st.session_state:
+                st.write('loop 1',st.session_state.cnt_loop_1)
+
+            if "cnt_loop_2" in st.session_state:
+                st.write('loop 2',st.session_state.cnt_loop_2)
 
         if tab2_df_output_data is not None:
             st.success("SEO-optimierte Produkttexte erfolgreich generiert.")
